f1f8beed1b56a8e6553e1d84003611a31576ff17
Kyrylo Yakymenko <yakimych@gmail.com>
2020-11-06 18:39:37 +0100

    Implement score selectors

--- reporter/AddResult.swift
+++ reporter/AddResult.swift
@@ -5,9 +5,63 @@ struct AddResult: View {
     @State var playerName: String
     @State var timerIsOpen: Bool = false
     
+    @State var playerPoints: String = "0"
+    @State var opponentPoints: String = "0"
+    @State var extraTime: Bool = false
+    
+    let maxSelectablePoints = 20
+    
+    func getColor(_ playerPointsString: String, _ currentNumber: Int) -> Color {
+        if Int(playerPointsString) == currentNumber {
+            return Color.green
+        }
+        return Color.yellow
+    }
+    
+    func setPlayerPoints(newValue: String) -> Void {
+        self.playerPoints = newValue
+    }
+    
     var body: some View {
-        VStack {
-            Text("Add Result against \(playerName) in \(communityName)")
+        VStack(alignment: .center) {
+            HStack {
+                VStack {
+                    Text("Player points: \(playerPoints)")
+                    TextField("Player", text: $playerPoints).keyboardType(.numberPad)
+                    ScrollView {
+                        VStack(spacing: 10) {
+                            ForEach(0..<maxSelectablePoints) { currentNumber in
+                                Button(String(currentNumber), action: { self.playerPoints = String(currentNumber) })
+                                    .foregroundColor(.blue)
+                                    .frame(width: 100, height: 50)
+                                    .background(getColor(self.playerPoints, currentNumber))
+                            }
+                        }
+                    }
+                }
+                VStack{
+                    Text("Opponent points: \(opponentPoints)")
+                    TextField("Opponent", text: $opponentPoints).keyboardType(.numberPad)
+                    ScrollView {
+                        VStack(spacing: 10) {
+                            ForEach(0..<maxSelectablePoints) { currentNumber in
+                                Button(String(currentNumber), action: { self.opponentPoints = String(currentNumber) })
+                                    .foregroundColor(.blue)
+                                    .frame(width: 100, height: 50)
+                                    .background(getColor(self.opponentPoints, currentNumber))
+                            }
+                        }
+                    }
+                }
+            }
+            Toggle(isOn: $extraTime, label: {
+                Text("Extra Time")
+            })
+            Button(action: {
+                print("TODO: Add result against \(playerName): \(playerPoints):\(opponentPoints) with extraTime: \(extraTime).")
+            }) {
+                Text("Add Result")
+            }
         }
         .navigationBarTitle("\(playerName) in \(communityName)")
         .navigationBarItems(trailing:
