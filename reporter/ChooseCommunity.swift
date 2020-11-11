334b649c364237b654008c0416e2924859ad2002
Kyrylo Yakymenko <yakimych@gmail.com>
2020-11-11 13:47:00 +0100

    Disallow deleting last community

--- reporter/ChooseCommunity.swift
+++ reporter/ChooseCommunity.swift
@@ -7,16 +7,21 @@ struct ChooseCommunity: View {
     @State private var isChoosingPlayer = false
 
     var body: some View {
+        let listContents =
+            ForEach(playersInCommunitiesStorage.items) {
+                communityWithPlayer in
+                Text("\(communityWithPlayer.playerName) (\(communityWithPlayer.communityName))")
+            }
+
         VStack {
             Text("Existing communities:")
-            List {
-                ForEach(playersInCommunitiesStorage.items) {
-                    communityWithPlayer in
-                    Text("\(communityWithPlayer.playerName) (\(communityWithPlayer.communityName))")
-                }
-                .onDelete { indexSet in
+
+            if playersInCommunitiesStorage.items.count <= 1 {
+                List { listContents }
+            } else {
+                List { listContents.onDelete(perform: { indexSet in
                     playersInCommunitiesStorage.items.remove(atOffsets: indexSet)
-                }
+                }) }
             }
 
             // TODO: Split this into a separate view, rename isChoosingPlayer to something that indicates that both player and community are being chosen
