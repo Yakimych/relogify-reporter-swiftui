0047974c092d59c57615166d841d45157cceb11c
Kyrylo Yakymenko <yakimych@gmail.com>
2020-11-12 17:00:10 +0100

    Move server url to settings file

--- reporter/Network.swift
+++ reporter/Network.swift
@@ -1,10 +1,21 @@
 import Foundation
 import Apollo
 
+struct Settings: Decodable {
+    let apiUrl: String
+}
+
 class Network {
     static let shared = Network()
 
-    private(set) lazy var apollo = ApolloClient(url: URL(string: "[TODO: Move to configuration (Plist? file]")!)
+    private lazy var apiUrl: () -> String = {
+        let url = Bundle.main.url(forResource: "Settings", withExtension: "plist")!
+        let data = try! Data(contentsOf: url)
+        let result = try! PropertyListDecoder().decode(Settings.self, from: data)
+        return result.apiUrl
+    }
+
+    private(set) lazy var apollo = ApolloClient(url: URL(string: apiUrl())!)
 }
 
 enum RemoteData<T> {
