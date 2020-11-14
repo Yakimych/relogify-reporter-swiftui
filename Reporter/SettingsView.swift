c2990f05579ed308b212e2b995c7952078939462
Kyrylo Yakymenko <yakimych@gmail.com>
2020-11-14 14:54:17 +0100

    Limit number of players in communities to 3

--- Reporter/SettingsView.swift
+++ Reporter/SettingsView.swift
@@ -1,8 +1,15 @@
 import SwiftUI
 
+
 struct SettingsView: View {
     @EnvironmentObject private var playersInCommunitiesStorage: PlayersInCommunitiesStorage
     @State private var isAddingPlayerInCommunity: Bool = false
+
+    private let maxNumberOfPlayersInCommunities = 3
+
+    private func canAddPlayerInCommunity() -> Bool {
+        return playersInCommunitiesStorage.items.count < maxNumberOfPlayersInCommunities
+    }
 
     var body: some View {
         let listContents =
@@ -13,6 +20,10 @@ struct SettingsView: View {
 
         NavigationView {
             VStack {
+                Text("Your communities")
+                    .font(.title)
+                    .padding()
+
                 List {
                     listContents.onDelete(perform: {
                         indexSet in playersInCommunitiesStorage.items.remove(atOffsets: indexSet)
@@ -27,6 +38,28 @@ struct SettingsView: View {
                     withIconButtonStyle(Image(systemName: "plus.circle.fill"))
                 }
                 .isDetailLink(false)
+                .disabled(!canAddPlayerInCommunity())
+
+                if !canAddPlayerInCommunity() {
+                    HStack {
+                        Image(systemName: "info.circle")
+                            .resizable()
+                            .foregroundColor(Color.blue)
+                            .frame(
+                                minWidth: 10,
+                                idealWidth: 30,
+                                maxWidth: 30,
+                                minHeight: 10,
+                                idealHeight: 30,
+                                maxHeight: 30,
+                                alignment: .center)
+
+                        Text("You can add \(maxNumberOfPlayersInCommunities) communities at most")
+                            .font(.footnote)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                    }
+                }
             }
             .navigationBarTitle("Settings", displayMode: .inline)
         }
