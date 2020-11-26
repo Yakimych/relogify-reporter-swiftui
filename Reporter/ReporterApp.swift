7f33fe7691c46b3ea1866d24b389f28913f1683e
Kyrylo Yakymenko <yakimych@gmail.com>
2020-11-26 14:32:45 +0100

    Register for notifications in AppDelegate

--- Reporter/ReporterApp.swift
+++ Reporter/ReporterApp.swift
@@ -1,13 +1,39 @@
 import SwiftUI
+import Firebase
 
 @main
 struct ReporterApp: App {
     @StateObject private var playersInCommunitiesStorage = PlayersInCommunitiesStorage()
 
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     var body: some Scene {
         WindowGroup {
             MainView()
                 .environmentObject(playersInCommunitiesStorage)
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+
+        return true
     }
 }
